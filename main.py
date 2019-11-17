c14f67789ff7c7719bb06c0a257fcf2ef05c8853
Ben Carpenter <carpenter.ben@icloud.com>
2019-11-17 17:10:34 -0600

    Correct pausing behaviour

--- main.py
+++ main.py
@@ -13,19 +13,23 @@ def get_input():
     global song_idx
     choice = input('[P]lay, P[a]use, [N]ext, [L]ast\n▶ ')
     if choice.lower() == 'p':
+        print("Playing...")
         player.play()
         return
     elif choice.lower() == 'a':
+        print("Pausing...")
         player.pause()
         get_input()
     elif choice.lower() == 'n':
-        player.stop()
+        print("Skipping...")
         song_idx += 1
+        player.set_media(song_list[song_idx])
         return
     elif choice.lower() == 'l':
+        print("Skipping back...")
         if (song_idx):
-            player.stop()
             song_idx -= 1
+            player.set_media(song_list[song_idx])
         else:
             player.stop()
             player.play()
@@ -37,8 +41,9 @@ for dirpath, dirnames, files in os.walk(music_path):
     for file in files:
         song_list.append(os.path.join(dirpath, file))
 
+player = vlc.MediaPlayer(song_list[song_idx])
+
 while (True):
-    player = vlc.MediaPlayer(song_list[song_idx])
     get_input()
     metadata = mutagen.File(song_list[song_idx], easy=True)
     print('Now playing \"' + str(metadata['title']).strip('[\']') 
